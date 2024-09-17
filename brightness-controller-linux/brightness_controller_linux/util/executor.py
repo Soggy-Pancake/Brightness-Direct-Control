798e157a7f05b0507d789ced464c45cf40137f92
Soggy_Pancake <soggy_pancake#8743>
2024-09-17 14:25:36 -0700

    added try block to xrandr execution as well.

--- brightness-controller-linux/brightness_controller_linux/util/executor.py
+++ brightness-controller-linux/brightness_controller_linux/util/executor.py
@@ -20,4 +20,7 @@ import subprocess
 
 
 def execute_command(string_cmd):
-    subprocess.check_output(string_cmd, shell=True)
+    try:
+        subprocess.check_output(string_cmd, shell=True)
+    except:
+        None
